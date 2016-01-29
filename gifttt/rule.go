3e5d9219805421a825f7ce0c150e198cee8c87a4
Tobias Heinzen <drtoful@users.noreply.github.com>
2016-01-29 20:24:25 +0100

    variable manager that persists values into db

--- gifttt/rule.go
+++ gifttt/rule.go
@@ -1,12 +1,62 @@
 package gifttt
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 
 	"github.com/drtoful/twik"
 	"github.com/drtoful/twik/ast"
 )
+
+var (
+	_manager  *VariableManager
+	varPrefix = "var~"
+)
+
+type VariableManager struct {
+}
+
+type Value struct {
+	Value interface{} `json:"value"`
+}
+
+func GetManager() *VariableManager {
+	if _manager == nil {
+		_manager = &VariableManager{}
+	}
+	return _manager
+}
+
+func (vm *VariableManager) Get(name string) (interface{}, error) {
+	store := GetStore()
+	b, err := store.Get(varPrefix + name)
+	if err != nil {
+		return nil, fmt.Errorf("undefined symbol: %s", name)
+	}
+
+	v := &Value{}
+	if err := json.Unmarshal([]byte(b), v); err == nil {
+		return v.Value, nil
+	} else {
+		return nil, err
+	}
+
+	panic("never reached")
+}
+
+func (vm *VariableManager) Set(name string, value interface{}) error {
+	v := &Value{Value: value}
+
+	b, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	store := GetStore()
+	return store.Set(varPrefix+name, string(b))
+}
 
 // the GlobalScope encapsulated over the DefaultScope of the LISP
 // interpreter. Get/Set will be delegated to it, so we can answer
@@ -20,11 +70,13 @@ func (s *GlobalScope) Create(symbol string, value interface{}) error {
 }
 
 func (s *GlobalScope) Set(symbol string, value interface{}) error {
-	return nil
+	manager := GetManager()
+	return manager.Set(symbol, value)
 }
 
 func (s *GlobalScope) Get(symbol string) (interface{}, error) {
-	return nil, nil
+	manager := GetManager()
+	return manager.Get(symbol)
 }
 
 func (s *GlobalScope) Branch() twik.Scope {
