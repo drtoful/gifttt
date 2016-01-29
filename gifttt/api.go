a3c9b2906a7bacadcec3e14a00a762a325729f1a
Tobias Heinzen <drtoful@users.noreply.github.com>
2016-01-29 23:55:18 +0100

    http api to set/get global variables

---
+++ gifttt/api.go
@@ -0,0 +1,101 @@
+package gifttt
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+)
+
+type APIServer struct {
+	ip, port string
+	handler  *negroni.Negroni
+}
+
+var (
+	internalVars = []string{
+		"time:second",
+		"time:minute",
+		"time:hour",
+		"date:day",
+		"date:month",
+		"date:year",
+	}
+)
+
+func postVar(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	vars := mux.Vars(r)
+	varname := vars["var"]
+
+	for _, c := range internalVars {
+		if c == varname {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+	}
+
+	var value Value
+	if err := json.NewDecoder(r.Body).Decode(&value); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	vm := GetManager()
+	if err := vm.Set(varname, value.Value); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func getVar(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	varname := vars["var"]
+
+	vm := GetManager()
+	v, err := vm.Get(varname)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	val := &Value{Value: v}
+	b, err := json.Marshal(val)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func NewAPIServer(ip, port string) *APIServer {
+	router := mux.NewRouter()
+
+	api := router.PathPrefix("/v").Subrouter()
+	api = api.StrictSlash(true)
+	api.Path("/{var}").Methods("POST").HandlerFunc(postVar)
+	api.Path("/{var}").Methods("GET").HandlerFunc(getVar)
+
+	n := negroni.New(negroni.NewRecovery())
+	n.UseHandler(router)
+
+	return &APIServer{
+		ip:      ip,
+		port:    port,
+		handler: n,
+	}
+}
+
+func (a *APIServer) Run() {
+	a.handler.Run(a.ip + ":" + a.port)
+}
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
+
+	"github.com/drtoful/gifttt/gifttt"
+)
+
+func main() {
+	var (
+		dbPath     = flag.String("db", "gifttt.db", "path to the database store")
+		rulePath   = flag.String("ruledir", "./", "path to rule files")
+		cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+		apiBind    = flag.String("ip", "", "ip to bind the api server to")
+		apiPort    = flag.String("port", "4200", "port for api server")
+	)
+	flag.Parse()
+
+	// activate cpu profiling
+	if *cpuprofile != "" {
+		log.Println("main: Starting CPU profiling '%s'", *cpuprofile)
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	if err := gifttt.StoreInit(*dbPath); err != nil {
+		log.Fatal(err)
+	}
+
+	// start rule manager
+	rm := gifttt.NewRuleManager(*rulePath)
+	go rm.Run()
+
+	// start the api server
+	api := gifttt.NewAPIServer(*apiBind, *apiPort)
+	go api.Run()
+
+	// all listeners are started in the background as
+	// gofunc's so we wait here for an interupt signal
+	// to stop the service gracefully
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case s := <-sig:
+			pprof.StopCPUProfile() // explicitly stop profiling
+			log.Fatalf("main: Signal (%d) received, stopping\n", s)
+		}
+	}
+}
